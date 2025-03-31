f9b0015c73e6b403c0e75867d8f920ab93cae922
Mieboymesias <161265927+Mieboymesias@users.noreply.github.com>
2025-03-31 15:25:58 +0800

    MAP
    MAP

---
+++ data/ACTIVITYTHISWEEK_10.js
@@ -0,0 +1,8 @@
+var json_ACTIVITYTHISWEEK_10 = {
+"type": "FeatureCollection",
+"name": "ACTIVITYTHISWEEK_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "TASK FORCE": "BONITE TASK FORCE", "ACTIVITIES": "DECLOGGING OF LINED CANAL ALONG GONZALES STREET", "LENGTH": "90m" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 125.489975117209553, 9.788583340006188 ], [ 125.490772957651728, 9.788369221198733 ] ] ] } }
+]
+}
---
+++ data/ACTIVITYTHISWEEK_9.js
@@ -0,0 +1,8 @@
+var json_ACTIVITYTHISWEEK_9 = {
+"type": "FeatureCollection",
+"name": "ACTIVITYTHISWEEK_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "photo": "D:\\HR REPORTS\\AS OF APRIL 4\\BO NITE LOADING OF WASTE MRF.jpg", "TASK FORCE": "BO NITE LOADING OF WASTE MRF", "longitude": "125.48833369444445", "latitude": "9.785663", "TASKFROCE": "TASK FORCE BONITE", "ACTIVITIES": "LOADING OF WASTE MATERIALS", "DATE": "2025-03-31" }, "geometry": { "type": "Point", "coordinates": [ 125.488333694444449, 9.785663 ] } }
+]
+}
